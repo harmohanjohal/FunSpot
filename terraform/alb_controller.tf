d82ae62539d1fd9233a58bf8586b7a4e6212df06
Harmohan Singh Johal <johalharmohan2121@gmail.com>
2026-03-04 19:00:20 +0000

    Fix syntax in Helm provider block

--- terraform/alb_controller.tf
+++ terraform/alb_controller.tf
@@ -16,10 +16,11 @@ module "load_balancer_controller_irsa_role" {
 
 # Helm Provider configuration to install the controller
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec {
+    
+    exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
       command     = "aws"
